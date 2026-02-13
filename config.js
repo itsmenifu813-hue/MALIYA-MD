f6650c4208a9ec284d162bb2b53e517da897942f
Maliya-bro <darkmaliya10@gmail.com>
2026-02-13 07:54:55 +0530

    Update SESSION_ID in config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "jxhGBS6S#M1nvT9tddDUMD7u-C8bRds31jq4YGS7eRKWE86ZLWyM",
+SESSION_ID: process.env.SESSION_ID || "P9o2WK6R#KXBpJHnwyoHjqMUJvqO7tBLmI4E4jO7FfRQ9rCllCbk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Maliya-bro/MALIYA-MD/blob/main/images/WhatsApp%20Image%202026-01-18%20at%2012.37.23.jpeg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALIYA-MD Is Alive Now😍😍😍.*",
 BOT_OWNER: "94702135392",
